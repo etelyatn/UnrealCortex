6e67dc30d1f4a52c64acc3a9d1a2ad7fc1290b44
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2026-02-10 17:16:30 +0100

    Wire CortexData to CortexCore command router

--- Plugins/UnrealCortex/Source/CortexData/Private/CortexDataModule.cpp
+++ Plugins/UnrealCortex/Source/CortexData/Private/CortexDataModule.cpp
@@ -1,10 +1,25 @@
 #include "CortexDataModule.h"
+#include "CortexCoreModule.h"
+#include "CortexCommandRouter.h"
+#include "CortexDataCommandHandler.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogCortexData, Log, All);
 
 void FCortexDataModule::StartupModule()
 {
     UE_LOG(LogCortexData, Log, TEXT("CortexData module starting up"));
+
+    FCortexCoreModule& CoreModule =
+        FModuleManager::GetModuleChecked<FCortexCoreModule>(TEXT("CortexCore"));
+
+    CoreModule.GetCommandRouter().SetDefaultHandler(
+        [](const FString& Command, const TSharedPtr<FJsonObject>& Params)
+        {
+            return FCortexDataCommandHandler::Execute(Command, Params);
+        }
+    );
+
+    UE_LOG(LogCortexData, Log, TEXT("CortexData registered %d commands with CortexCore"), 29);
 }
 
 void FCortexDataModule::ShutdownModule()
