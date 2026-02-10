c6ba8b2656d063e77fa32e2b5ed89f130b78984a
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2026-02-10 17:04:59 +0100

    Wire CortexCore module: TCP server + command router with external dispatch

--- Plugins/UnrealCortex/Source/CortexCore/Private/CortexCoreModule.cpp
+++ Plugins/UnrealCortex/Source/CortexCore/Private/CortexCoreModule.cpp
@@ -1,15 +1,50 @@
 #include "CortexCoreModule.h"
+#include "CortexCommandRouter.h"
+#include "CortexTcpServer.h"
+#include "CortexSettings.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogCortex, Log, All);
 
 void FCortexCoreModule::StartupModule()
 {
     UE_LOG(LogCortex, Log, TEXT("CortexCore module starting up"));
+
+    const UUDBSettings* Settings = UUDBSettings::Get();
+    if (Settings == nullptr || !Settings->bAutoStart)
+    {
+        return;
+    }
+
+    CommandRouter = MakeUnique<FUDBCommandHandler>();
+
+    TcpServer = MakeUnique<FUDBTcpServer>();
+    TcpServer->Start(Settings->Port,
+        [this](const FString& Command, const TSharedPtr<FJsonObject>& Params)
+        {
+            return CommandRouter->Execute(Command, Params);
+        }
+    );
+
+    UE_LOG(LogCortex, Log, TEXT("CortexCore TCP server started on port %d"), Settings->Port);
 }
 
 void FCortexCoreModule::ShutdownModule()
 {
     UE_LOG(LogCortex, Log, TEXT("CortexCore module shutting down"));
+
+    if (TcpServer.IsValid())
+    {
+        TcpServer->Stop();
+        TcpServer.Reset();
+    }
+
+    CommandRouter.Reset();
+}
+
+FUDBCommandHandler& FCortexCoreModule::GetCommandRouter()
+{
+    check(CommandRouter.IsValid());
+    return *CommandRouter;
 }
 
 IMPLEMENT_MODULE(FCortexCoreModule, CortexCore)
--- Plugins/UnrealCortex/Source/CortexCore/Public/CortexCoreModule.h
+++ Plugins/UnrealCortex/Source/CortexCore/Public/CortexCoreModule.h
@@ -3,9 +3,19 @@
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
 
+class FUDBCommandHandler;
+class FUDBTcpServer;
+
 class FCortexCoreModule : public IModuleInterface
 {
 public:
     virtual void StartupModule() override;
     virtual void ShutdownModule() override;
+
+    /** Get the command router for domain modules to register handlers. */
+    FUDBCommandHandler& GetCommandRouter();
+
+private:
+    TUniquePtr<FUDBCommandHandler> CommandRouter;
+    TUniquePtr<FUDBTcpServer> TcpServer;
 };
