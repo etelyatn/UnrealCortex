bd29368f468ca2fd1bb41d76ccb4196b9a153205
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2026-02-11 15:20:21 +0100

    test(blueprint): add bp.create types test

---
+++ Plugins/UnrealCortex/Source/CortexBlueprintTests/Private/CortexBPCreateTypesTest.cpp
@@ -0,0 +1,236 @@
+// Copyright Andrei Sudarikov. All Rights Reserved.
+
+#include "Misc/AutomationTest.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "Engine/Blueprint.h"
+#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CortexBPCommandHandler.h"
+#include "CortexTypes.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogCortexBPTypesTests, Log, All);
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPCreateActorTypeTest,
+	"Cortex.Blueprint.Create.Types.Actor",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPCreateActorTypeTest::RunTest(const FString& Parameters)
+{
+	// Setup
+	FCortexBPCommandHandler Handler;
+	TSharedPtr<FJsonObject> Params = MakeShared<FJsonObject>();
+	Params->SetStringField(TEXT("name"), TEXT("BP_TestActor"));
+	Params->SetStringField(TEXT("path"), TEXT("/Temp/CortexBPTest_Types"));
+	Params->SetStringField(TEXT("type"), TEXT("Actor"));
+
+	// Execute
+	FCortexCommandResult Result = Handler.Execute(TEXT("create"), Params);
+
+	// Verify
+	TestTrue(TEXT("Command should succeed"), Result.bSuccess);
+
+	// Verify asset is loadable
+	UBlueprint* LoadedBP = LoadObject<UBlueprint>(nullptr, TEXT("/Game/Temp/CortexBPTest_Types/BP_TestActor"));
+	TestNotNull(TEXT("Asset should be loadable"), LoadedBP);
+
+	if (LoadedBP)
+	{
+		// Verify parent class is AActor
+		UClass* ParentClass = LoadedBP->ParentClass;
+		TestNotNull(TEXT("Parent class should exist"), ParentClass);
+		if (ParentClass)
+		{
+			TestTrue(TEXT("Parent class should be or derive from AActor"),
+				ParentClass->IsChildOf(AActor::StaticClass()));
+		}
+
+		// Cleanup
+		LoadedBP->MarkAsGarbage();
+	}
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPCreateComponentTypeTest,
+	"Cortex.Blueprint.Create.Types.Component",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPCreateComponentTypeTest::RunTest(const FString& Parameters)
+{
+	// Setup
+	FCortexBPCommandHandler Handler;
+	TSharedPtr<FJsonObject> Params = MakeShared<FJsonObject>();
+	Params->SetStringField(TEXT("name"), TEXT("BP_TestComponent"));
+	Params->SetStringField(TEXT("path"), TEXT("/Temp/CortexBPTest_Types"));
+	Params->SetStringField(TEXT("type"), TEXT("Component"));
+
+	// Execute
+	FCortexCommandResult Result = Handler.Execute(TEXT("create"), Params);
+
+	// Verify
+	TestTrue(TEXT("Command should succeed"), Result.bSuccess);
+
+	// Verify asset is loadable
+	UBlueprint* LoadedBP = LoadObject<UBlueprint>(nullptr, TEXT("/Game/Temp/CortexBPTest_Types/BP_TestComponent"));
+	TestNotNull(TEXT("Asset should be loadable"), LoadedBP);
+
+	if (LoadedBP)
+	{
+		// Verify parent class is UActorComponent
+		UClass* ParentClass = LoadedBP->ParentClass;
+		TestNotNull(TEXT("Parent class should exist"), ParentClass);
+		if (ParentClass)
+		{
+			TestTrue(TEXT("Parent class should be or derive from UActorComponent"),
+				ParentClass->IsChildOf(UActorComponent::StaticClass()));
+		}
+
+		// Cleanup
+		LoadedBP->MarkAsGarbage();
+	}
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPCreateFunctionLibraryTypeTest,
+	"Cortex.Blueprint.Create.Types.FunctionLibrary",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPCreateFunctionLibraryTypeTest::RunTest(const FString& Parameters)
+{
+	// Setup
+	FCortexBPCommandHandler Handler;
+	TSharedPtr<FJsonObject> Params = MakeShared<FJsonObject>();
+	Params->SetStringField(TEXT("name"), TEXT("BP_TestFunctionLibrary"));
+	Params->SetStringField(TEXT("path"), TEXT("/Temp/CortexBPTest_Types"));
+	Params->SetStringField(TEXT("type"), TEXT("FunctionLibrary"));
+
+	// Execute
+	FCortexCommandResult Result = Handler.Execute(TEXT("create"), Params);
+
+	// Verify
+	TestTrue(TEXT("Command should succeed"), Result.bSuccess);
+
+	// Verify asset is loadable
+	UBlueprint* LoadedBP = LoadObject<UBlueprint>(nullptr, TEXT("/Game/Temp/CortexBPTest_Types/BP_TestFunctionLibrary"));
+	TestNotNull(TEXT("Asset should be loadable"), LoadedBP);
+
+	if (LoadedBP)
+	{
+		// Verify parent class is UBlueprintFunctionLibrary
+		UClass* ParentClass = LoadedBP->ParentClass;
+		TestNotNull(TEXT("Parent class should exist"), ParentClass);
+		if (ParentClass)
+		{
+			TestTrue(TEXT("Parent class should be or derive from UBlueprintFunctionLibrary"),
+				ParentClass->IsChildOf(UBlueprintFunctionLibrary::StaticClass()));
+		}
+
+		// Cleanup
+		LoadedBP->MarkAsGarbage();
+	}
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPCreateInterfaceTypeTest,
+	"Cortex.Blueprint.Create.Types.Interface",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPCreateInterfaceTypeTest::RunTest(const FString& Parameters)
+{
+	// Setup
+	FCortexBPCommandHandler Handler;
+	TSharedPtr<FJsonObject> Params = MakeShared<FJsonObject>();
+	Params->SetStringField(TEXT("name"), TEXT("BP_TestInterface"));
+	Params->SetStringField(TEXT("path"), TEXT("/Temp/CortexBPTest_Types"));
+	Params->SetStringField(TEXT("type"), TEXT("Interface"));
+
+	// Execute
+	FCortexCommandResult Result = Handler.Execute(TEXT("create"), Params);
+
+	// Verify
+	TestTrue(TEXT("Command should succeed"), Result.bSuccess);
+
+	// Verify asset is loadable
+	UBlueprint* LoadedBP = LoadObject<UBlueprint>(nullptr, TEXT("/Game/Temp/CortexBPTest_Types/BP_TestInterface"));
+	TestNotNull(TEXT("Asset should be loadable"), LoadedBP);
+
+	if (LoadedBP)
+	{
+		// Verify parent class is UInterface
+		UClass* ParentClass = LoadedBP->ParentClass;
+		TestNotNull(TEXT("Parent class should exist"), ParentClass);
+		if (ParentClass)
+		{
+			TestTrue(TEXT("Parent class should be or derive from UInterface"),
+				ParentClass->IsChildOf(UInterface::StaticClass()));
+		}
+
+		// Cleanup
+		LoadedBP->MarkAsGarbage();
+	}
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPCreateWidgetTypeTest,
+	"Cortex.Blueprint.Create.Types.Widget",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPCreateWidgetTypeTest::RunTest(const FString& Parameters)
+{
+	// Setup
+	FCortexBPCommandHandler Handler;
+	TSharedPtr<FJsonObject> Params = MakeShared<FJsonObject>();
+	Params->SetStringField(TEXT("name"), TEXT("WBP_TestWidget"));
+	Params->SetStringField(TEXT("path"), TEXT("/Temp/CortexBPTest_Types"));
+	Params->SetStringField(TEXT("type"), TEXT("Widget"));
+
+	// Execute
+	FCortexCommandResult Result = Handler.Execute(TEXT("create"), Params);
+
+	// Verify
+	TestTrue(TEXT("Command should succeed"), Result.bSuccess);
+
+	// Verify asset is loadable
+	UBlueprint* LoadedBP = LoadObject<UBlueprint>(nullptr, TEXT("/Game/Temp/CortexBPTest_Types/WBP_TestWidget"));
+	TestNotNull(TEXT("Asset should be loadable"), LoadedBP);
+
+	if (LoadedBP)
+	{
+		// Verify parent class is UserWidget (UMG)
+		UClass* ParentClass = LoadedBP->ParentClass;
+		TestNotNull(TEXT("Parent class should exist"), ParentClass);
+		if (ParentClass)
+		{
+			// UserWidget class is dynamically resolved - check by name
+			UClass* UserWidgetClass = FindObject<UClass>(nullptr, TEXT("/Script/UMG.UserWidget"));
+			if (UserWidgetClass)
+			{
+				TestTrue(TEXT("Parent class should be or derive from UserWidget"),
+					ParentClass->IsChildOf(UserWidgetClass));
+			}
+			else
+			{
+				AddWarning(TEXT("UserWidget class not available (UMG module may not be loaded)"));
+			}
+		}
+
+		// Cleanup
+		LoadedBP->MarkAsGarbage();
+	}
+
+	return true;
+}
