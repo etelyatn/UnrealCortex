f00db52e20cac908976988c32a99d9ee88b25bc6
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2026-02-26 21:50:44 +0100

    test(blueprint): cover rename swap workflow

--- Source/CortexBlueprint/Private/Tests/CortexBPRenameTest.cpp
+++ Source/CortexBlueprint/Private/Tests/CortexBPRenameTest.cpp
@@ -3,6 +3,8 @@
 #include "CortexTypes.h"
 #include "Dom/JsonObject.h"
 #include "Engine/Blueprint.h"
+#include "Misc/Guid.h"
+#include "Misc/PackageName.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
 	FCortexBPRenameBasicTest,
@@ -12,12 +14,15 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(
 
 bool FCortexBPRenameBasicTest::RunTest(const FString& Parameters)
 {
-	const FString SourcePath = TEXT("/Game/Temp/CortexBPTest_Rename/BP_Rename_Source");
-	const FString DestPath = TEXT("/Game/Temp/CortexBPTest_Rename/BP_Rename_Dest");
+	const FString Suffix = FGuid::NewGuid().ToString(EGuidFormats::Digits).Left(8);
+	const FString FolderPath = FString::Printf(TEXT("/Game/Temp/BPRenameFlow_%s"), *Suffix);
+	const FString SourcePath = FString::Printf(TEXT("%s/BP_Rename_Source_%s"), *FolderPath, *Suffix);
+	const FString DestPath = FString::Printf(TEXT("%s/BP_Rename_Dest_%s"), *FolderPath, *Suffix);
+	const FString SourceName = FPackageName::GetShortName(SourcePath);
 
 	TSharedPtr<FJsonObject> CreateParams = MakeShared<FJsonObject>();
-	CreateParams->SetStringField(TEXT("name"), TEXT("BP_Rename_Source"));
-	CreateParams->SetStringField(TEXT("path"), TEXT("/Game/Temp/CortexBPTest_Rename"));
+	CreateParams->SetStringField(TEXT("name"), SourceName);
+	CreateParams->SetStringField(TEXT("path"), FolderPath);
 	CreateParams->SetStringField(TEXT("type"), TEXT("Actor"));
 	const FCortexCommandResult CreateResult = FCortexBPAssetOps::Create(CreateParams);
 	TestTrue(TEXT("Source BP created"), CreateResult.bSuccess);
@@ -32,9 +37,51 @@ bool FCortexBPRenameBasicTest::RunTest(const FString& Parameters)
 	UBlueprint* RenamedBP = FCortexBPAssetOps::LoadBlueprint(DestPath, LoadError);
 	TestNotNull(TEXT("Blueprint loadable at new path"), RenamedBP);
 
-	TSharedPtr<FJsonObject> DeleteParams = MakeShared<FJsonObject>();
-	DeleteParams->SetStringField(TEXT("asset_path"), DestPath);
-	FCortexBPAssetOps::Delete(DeleteParams);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FCortexBPRenameBatchSwapTest,
+	"Cortex.Blueprint.Rename.BatchSwap",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FCortexBPRenameBatchSwapTest::RunTest(const FString& Parameters)
+{
+	const FString Suffix = FGuid::NewGuid().ToString(EGuidFormats::Digits).Left(8);
+	const FString FolderPath = FString::Printf(TEXT("/Game/Temp/BPRenameSwap_%s"), *Suffix);
+	const FString APath = FString::Printf(TEXT("%s/BP_A_%s"), *FolderPath, *Suffix);
+	const FString BPath = FString::Printf(TEXT("%s/BP_B_%s"), *FolderPath, *Suffix);
+	const FString BackupPath = FString::Printf(TEXT("%s/BP_A_Backup_%s"), *FolderPath, *Suffix);
+
+	TSharedPtr<FJsonObject> CreateA = MakeShared<FJsonObject>();
+	CreateA->SetStringField(TEXT("name"), FPackageName::GetShortName(APath));
+	CreateA->SetStringField(TEXT("path"), FolderPath);
+	CreateA->SetStringField(TEXT("type"), TEXT("Actor"));
+	TestTrue(TEXT("Create A succeeded"), FCortexBPAssetOps::Create(CreateA).bSuccess);
+
+	TSharedPtr<FJsonObject> CreateB = MakeShared<FJsonObject>();
+	CreateB->SetStringField(TEXT("name"), FPackageName::GetShortName(BPath));
+	CreateB->SetStringField(TEXT("path"), FolderPath);
+	CreateB->SetStringField(TEXT("type"), TEXT("Actor"));
+	TestTrue(TEXT("Create B succeeded"), FCortexBPAssetOps::Create(CreateB).bSuccess);
+
+	TSharedPtr<FJsonObject> RenameA = MakeShared<FJsonObject>();
+	RenameA->SetStringField(TEXT("source_path"), APath);
+	RenameA->SetStringField(TEXT("dest_path"), BackupPath);
+	TestTrue(TEXT("Rename A->Backup succeeded"), FCortexBPAssetOps::Rename(RenameA).bSuccess);
+
+	TSharedPtr<FJsonObject> RenameB = MakeShared<FJsonObject>();
+	RenameB->SetStringField(TEXT("source_path"), BPath);
+	RenameB->SetStringField(TEXT("dest_path"), APath);
+	TestTrue(TEXT("Rename B->A succeeded"), FCortexBPAssetOps::Rename(RenameB).bSuccess);
+
+	FString LoadError;
+	UBlueprint* BackupBP = FCortexBPAssetOps::LoadBlueprint(BackupPath, LoadError);
+	TestNotNull(TEXT("A backup exists"), BackupBP);
+
+	UBlueprint* NewABP = FCortexBPAssetOps::LoadBlueprint(APath, LoadError);
+	TestNotNull(TEXT("A now points to former B"), NewABP);
 
 	return true;
 }
