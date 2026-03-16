071b5ff1d8d1f0779800c4e1b71545c9abddf7a5
Eugene Telyatnik <eugene.telyatnik@gmail.com>
2026-03-16 19:46:37 +0100

    build(qa): add Slate/SlateCore for IInputProcessor support

--- Source/CortexQA/CortexQA.Build.cs
+++ Source/CortexQA/CortexQA.Build.cs
@@ -23,6 +23,8 @@ public class CortexQA : ModuleRules
             "AIModule",
             "GameplayTags",
             "InputCore",
+            "Slate",
+            "SlateCore",
             "CortexEditor",
         });
     }
